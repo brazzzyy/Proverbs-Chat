f0939aa393bb84f1fad09a4636fc9505d323b77a
Brady Lor <lor528152@outlook.com>
2025-09-04 00:11:27 -0500

    fixed environment variables in deployment

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -6,7 +6,7 @@ import { createClient } from "@supabase/supabase-js";
 export async function POST(req: NextRequest) {
 
     const supabase = createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.SUPABASE_URL!,
         process.env.SUPABASE_SERVICE_ROLE_KEY!
     );
 
